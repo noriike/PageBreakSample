b8b70a02d66bb65fdf4043e8e794355bc0251b8f
noriike <hidikeda7630@gmail.com>
2015-05-07 22:56:59 +0900

    PageBreak

---
+++ WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+
+    <style>
+        .pageblock {
+            page-break-after:always;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Panel ID="Panel1" runat="server"></asp:Panel>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.SetUp();
+        }
+
+        private void SetUp()
+        {
+            
+            
+            for(int i=0;i<=1;i++)
+            {
+                this.Makepage(i);
+            }
+
+           
+        }
+
+        private void Makepage(int i)
+        {
+            Panel pageBlock = new Panel();
+            pageBlock.CssClass = "pageblock";
+
+            Label l = new Label();
+            l.Text ="Page:" +  i.ToString();
+            pageBlock.Controls.Add(l);
+
+            this.Panel1.Controls.Add(pageBlock);
+        }
+    }
+}
